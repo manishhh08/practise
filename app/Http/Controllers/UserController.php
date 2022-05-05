01f006c1f5aa91c3851dae6c5a17ba195025da2f
manish <manish.lama0321@gmail.com>
2022-05-05 18:06:17 +1000

    practise site

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+    public function index(){
+
+    }
+    public function addUser(){
+
+    }
+    public function deleteUser(){
+
+    }
+}
