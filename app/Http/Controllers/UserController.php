f0d9600fd75bfef268f01a2af9e7729cc2e98699
manish <manish.lama0321@gmail.com>
2022-05-05 18:17:11 +1000

    a

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\User;//similar to include
+
 
 
 class UserController extends Controller
 {
     public function index(){
-
+        echo 'here i m';
+        exit;
     }
     public function addUser(){
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,15 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function () {
-    return view('home');
-});
+
 Route::get('/display',function(){
     return view('display');
 });
-Route::get('/home',function(){
-    return view('home');
-});
+Route::get('/home',[UserController::class,'index']);
