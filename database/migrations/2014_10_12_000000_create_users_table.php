88a8b23872f11af7a0e4e6583e00d7917584ffe0
manish <manish.lama0321@gmail.com>
2022-05-06 19:13:02 +1000

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,17 +17,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('lname');
+            $table->string('mname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+    
+        }
 
     /**
      * Reverse the migrations.
